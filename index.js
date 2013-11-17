b07f4c20f454f148035aabda47766bc37a829924
Michal Miskernik <michal.miskernik@gmail.com>
2013-11-17 13:00:47 +0100

    added global error handler

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+var bind = require('bind');
 var Backend = require('./backend.js');
 var browser = require('./detect.js');
 
 module.exports = {
   backend: new Backend(),
   project: null,
+  reported: [],
 
   setup: function(project) {
     this.project = project;
     this.backend.init();
-    
+
+    window.onerror = bind(this, 'handle');
+
     return this;
   },
 
@@ -20,6 +24,16 @@ module.exports = {
       stack: err.stack
     });
 
+    this.reported.push(err);
     this.backend.send(json);
-  }  
+  },
+
+  handle: function() {
+    var err = arguments[4];
+    if (err == null) return;
+
+    if (this.reported.indexOf(err) < 0) {
+      this.report(err);
+    }
+  }
 };
