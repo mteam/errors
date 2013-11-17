6ff48eba8fa268ece819b11fe95400c46390fb1a
Michal Miskernik <michal.miskernik@gmail.com>
2013-11-17 13:18:17 +0100

    improved global handler

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var browser = require('./detect.js');
 module.exports = {
   backend: new Backend(),
   project: null,
-  reported: [],
+  reported: false,
 
   setup: function(project) {
     this.project = project;
@@ -17,23 +17,31 @@ module.exports = {
   },
 
   report: function(err) {
-    var json = JSON.stringify({
-      project: this.project,
-      message: err.message,
-      browser: browser,
-      stack: err.stack
-    });
-
-    this.reported.push(err);
-    this.backend.send(json);
+    this.send(err.message, err.stack);
+    this.reported = true;
   },
 
-  handle: function() {
-    var err = arguments[4];
-    if (err == null) return;
-
-    if (this.reported.indexOf(err) < 0) {
-      this.report(err);
+  handle: function(message, url, line, column, error) {
+    if (this.reported) {
+      this.reported = false;
+      return;
     }
+
+    var stack = error
+      ? error.stack
+      : url + ':' + line;
+
+    this.send(message, stack);
+  },
+
+  send: function(message, stack) {
+    var json = JSON.stringify({
+      project: this.project,
+      message: message,
+      browser: browser,
+      stack: stack
+    });
+
+    this.backend.send(json);
   }
 };
